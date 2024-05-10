469aeec9f2db49bf06fc518da81178b0f620206d
sores <drewmax1234@yahoo.com>
2024-05-10 01:42:44 -0400

    Add colors to guide

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -5,9 +5,9 @@ const Guide = () => {
     <div className='flex flex-col text-center items-center justify-center gap-2'>
         <h3 className='text-lg text-white bg-slate-600 underline border-2 border-zinc-700 rounded-lg px-12 py-2 mb-2'>BMI Weight Guide</h3>
         <div className='mt-3'>
-          <p>Less than 18.6% BMI = Under Weight</p>
-          <p>18.6% - 24.9% BMI = Normal Range</p>
-          <p>24.9% or Greater = Over Weight</p>
+          <p id='guide-p1'>Less than 18.6% BMI = <span>Under Weight</span></p>
+          <p id='guide-p2'>18.6% - 24.9% BMI = <span>Normal Range</span></p>
+          <p id='guide-p3'>24.9% or Greater = <span>Over Weight</span></p>
         </div>
     </div>
   )
