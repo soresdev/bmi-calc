37e54e751ce6df79795c76d8a5eafb3080462b1b
sores <drewmax1234@yahoo.com>
2024-05-15 20:38:16 -0400

    Use tailwind for guide span styles

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -5,9 +5,9 @@ const Guide = () => {
     <div className='flex flex-col text-center items-center justify-center gap-2'>
         <h3 className='text-xl text-white bg-slate-600 border-2 border-zinc-700 rounded-lg px-20 py-2 mb-2'>BMI Weight Guide</h3>
         <div className='mt-1'>
-          <p id='guide-p1'>Less than 18.6% BMI = <span>Under Weight</span></p>
-          <p id='guide-p2'>18.6% - 24.9% BMI = <span>Normal Range</span></p>
-          <p id='guide-p3'>24.9% or Greater = <span>Over Weight</span></p>
+          <p>Less than 18.6% BMI = <span className='text-orange-400 font-semibold'>Under Weight</span></p>
+          <p>18.6% - 24.9% BMI = <span className='text-green-700 font-semibold'>Normal Range</span></p>
+          <p>24.9% or Greater = <span className='text-red-600 font-semibold'>Over Weight</span></p>
         </div>
     </div>
   )
