8c26f929cbbc1c1c2f5d21c549f5d88c8d474544
sores <drewmax1234@yahoo.com>
2024-12-18 20:37:37 -0500

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,8 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/jsx-uses-react": "error",   
+    "react/jsx-uses-vars": "error",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
